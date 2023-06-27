b410de30e470e3094b753b439916e53564981a8d
DarkMetalThrone <darkmetalthrone@gmail.com>
2023-06-27 20:38:32 +0530

    first commit

---
+++ Day16/newton.session.sql
@@ -0,0 +1,14 @@
+{
+  "mysqlOptions": {
+    "authProtocol": "xprotocol"
+  },
+  "previewLimit": 50,
+  "server": "localhost",
+  "port": 33060,
+  "driver": "MySQL",
+  "name": "Newton",
+  "database": "newton",
+  "username": "root",
+  "password": "1234",
+  "connectionTimeout": 100
+}
